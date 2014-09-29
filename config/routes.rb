d2c785bb203766b78052ac3f4f3317859196d033
Tim Smith <smith@ttimsmith.com>
2014-09-29 12:52:06 -0500

    New routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#dashboard'
 
   # Only generate root devise routes for sessions (sign in / sign out)
-  devise_for :users, only: :sessions
+  devise_for :users, only: [:passwords, :sessions], path: '', path_names: { sign_in: 'login' }
 
   namespace :admin do
     root to: 'users#index'
