fce097f28400a5ac6e8169cbe112c84455f4b47b
Tim Smith <smith@ttimsmith.com>
2014-10-07 16:44:22 -0500

    Creating attachment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001213248) do
+ActiveRecord::Schema.define(version: 20141007214337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachments", force: true do |t|
+    t.string   "file_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "body"
