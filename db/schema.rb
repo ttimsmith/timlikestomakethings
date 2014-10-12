cdb0c0a98a08f3cfc758cfba4bd891dcbe4c8a17
Tim Smith <smith@ttimsmith.com>
2014-10-12 15:01:41 -0500

    Add plan id to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001213248) do
+ActiveRecord::Schema.define(version: 20141012200049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachments", force: true do |t|
+    t.string   "file_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "post_id"
+    t.integer  "comment_id"
+  end
+
+  add_index "attachments", ["comment_id"], name: "index_attachments_on_comment_id", using: :btree
+  add_index "attachments", ["post_id"], name: "index_attachments_on_post_id", using: :btree
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -71,9 +82,11 @@ ActiveRecord::Schema.define(version: 20141001213248) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["plan_id"], name: "index_users_on_plan_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 
