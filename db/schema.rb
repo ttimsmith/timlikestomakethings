3030db236817949e09340f3396bdb4293952e0c7
Tim Smith <smith@ttimsmith.com>
2014-10-13 16:51:48 -0500

    Adding customer id and last 4 to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013192014) do
+ActiveRecord::Schema.define(version: 20141013213129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 20141013192014) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.integer  "plan_id"
+    t.string   "customer_id"
+    t.string   "last_4_digits"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
